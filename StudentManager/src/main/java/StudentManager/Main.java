7dc991e30bea30c066a6201940cfd02d26b7b748
Nicolae_Semionov <nicolae@my.yorku.ca>
2021-05-03 01:04:35 -0400

    Javafx classpath and basic window

--- StudentManager/src/main/java/StudentManager/Main.java
+++ StudentManager/src/main/java/StudentManager/Main.java
@@ -3,10 +3,36 @@ package StudentManager;
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.io.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 	
 	public static void main(String[] args) {
+		
+		launch(args);
+		
+
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Student Manager");
+		
+		StackPane layout = new StackPane();
+		
+		Scene scene = new Scene(layout, 300, 250);
+		
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
+		
 		
 		ArrayList Students = Data.load("C:/Users/nicol/git/StudentManager/StudentManager/test.txt");
 		
@@ -18,6 +44,5 @@ public class Main {
 		Students.add(new Student("Jeff", "Valantine", 355));
 		
 		Data.save(Students, "C:/Users/nicol/git/StudentManager/StudentManager/test.txt");
-
 	}
 }
